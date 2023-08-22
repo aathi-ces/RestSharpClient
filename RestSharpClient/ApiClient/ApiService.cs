92e26ebee35d215156108dca11fa7731bf5e331e
Aathiraja Krishnamoorthy <aathiraja.krishnamoorthy@cesltd.com>
2023-08-23 00:38:46 +0530

    framework basic setup and basic tests added with required helpers

---
+++ RestSharpClient/ApiClient/ApiService.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+using System.Reflection.PortableExecutable;
+using System.Net.Mime;
+using RestSharp.Authenticators;
+using RestSharpClient.Model.RestfulBooker;
+using Settings;
+
+namespace RestSharpClient.ApiClient
+{
+    public partial class ApiService : IApiService, IDisposable
+    {
+        readonly RestClient restClient;
+        public RestResponse? Response { get; set; }
+
+        public ApiService(Uri baseUrl) {
+
+            RestClientOptions restClientOptions = new()
+            {
+                BaseUrl = baseUrl,
+                Authenticator = new HttpBasicAuthenticator("admin", "password123")
+            };
+            restClient = new (restClientOptions);
+        }
+
+        public IApiService PostMethod(object requestObject, string uri, Dictionary<string, string>? headers = null)
+        {
+
+            var request = new RestRequest() { Resource = uri, Method = Method.Post, RequestFormat = DataFormat.Json };
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            Response = Execute(restClient, request, requestObject, headers!);
+            return this;
+        }
+
+        public IApiService GetMethod(string uri, Dictionary<string, string>? headers = null)
+        {
+            var request = new RestRequest() { Resource = uri, Method = Method.Get, RequestFormat = DataFormat.Json };
+            Response = Execute(restClient, request, headers);
+            return this;
+        }
+
+        public static RestRequest SetRequestElements(RestRequest request, ApiMethodParams apiMethodParams) {
+            request.AddHeader("Accept", MediaTypeNames.Application.Json);
+            request.AddHeader("Content-Type", MediaTypeNames.Application.Json);
+            apiMethodParams.Headers?.Where(l => l.Key != null).ToList().ForEach(header => request.AddHeader(header.Key, header.Value));
+            apiMethodParams.UrlSegments?.Where(l => l.Key != null).ToList().ForEach(segment => request.AddUrlSegment(segment.Key, segment.Value));
+            apiMethodParams.QueryParams?.Where(l => l.Key != null).ToList().ForEach(param => request.AddQueryParameter(param.Key, param.Value));
+            return request;
+        }
+
+        public IApiService GetMethod(ApiMethodParams apiMethodParams)
+        {
+            var request = new RestRequest() { Resource = apiMethodParams.Uri!, Method = Method.Get, RequestFormat = DataFormat.Json };
+            SetRequestElements(request, apiMethodParams);
+            Response = Execute(restClient, request, apiMethodParams.Headers);
+            return this;
+        }
+
+        public IApiService PutMethod(object requestObject, string uri, Dictionary<string, string>? headers = null)
+        {
+            var request = new RestRequest() { Resource = uri, Method = Method.Put, RequestFormat = DataFormat.Json };
+            Response = Execute(restClient, request, requestObject, headers!);
+            return this;
+        }
+
+        public IApiService PutMethod(ApiMethodParams apiMethodParams)
+        {
+            var request = new RestRequest() { Resource = apiMethodParams.Uri!, Method = Method.Put, RequestFormat = DataFormat.Json };
+            request = SetRequestElements(request, apiMethodParams);
+            Response = Execute(restClient, request, apiMethodParams.Payload, apiMethodParams.Headers!);
+            return this;
+        }
+
+        public IApiService PatchMethod(object requestObject, string uri, Dictionary<string, string>? headers = null)
+        {
+            var request = new RestRequest() { Resource = uri, Method = Method.Patch, RequestFormat = DataFormat.Json };
+            Response = Execute(restClient, request, requestObject, headers!);
+            return this;
+        }
+
+        public IApiService PatchMethod(ApiMethodParams apiMethodParams)
+        {
+            var request = new RestRequest() { Resource = apiMethodParams.Uri!, Method = Method.Patch, RequestFormat = DataFormat.Json };
+            request = SetRequestElements(request, apiMethodParams);
+            Response = Execute(restClient, request, apiMethodParams.Payload, apiMethodParams.Headers!);
+            return this;
+        }
+
+        public IApiService DeleteMethod(string uri, Dictionary<string, string>? headers = null)
+        {
+            var request = new RestRequest() { Resource = uri, Method = Method.Delete, RequestFormat = DataFormat.Json };
+            Response = Execute(restClient, request, null, headers!);
+            return this;
+        }
+
+        public IApiService DeleteMethod(ApiMethodParams apiMethodParams)
+        {
+            var request = new RestRequest() { Resource = apiMethodParams.Uri!, Method = Method.Delete, RequestFormat = DataFormat.Json };
+            request = SetRequestElements(request, apiMethodParams);
+            Response = Execute(restClient, request, apiMethodParams.Payload, apiMethodParams.Headers!);
+            return this;
+        }
+
+        private static RestResponse Execute(RestClient client, RestRequest request, object? requestObject = null, Dictionary<string, string>? headers = null)
+        {
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                    request.AddHeader(header.Key, header.Value);
+            }
+
+            if (requestObject != null)
+                request.AddJsonBody(requestObject);
+
+            return client.Execute(request);
+        }
+
+        public T GetValue<T>()
+        {
+            return JsonConvert.DeserializeObject<T>(Response!.Content!, Converter.Settings)!;
+        }
+
+        public List<T> GetValueInList<T>()
+        {
+            return JsonConvert.DeserializeObject<List<T>>(Response!.Content!)!;
+        }
+
+        public RestResponse GetResponse()
+        {
+            return Response!;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class ApiService<T>
+    {
+        public static T FromJson(string json) => JsonConvert.DeserializeObject<T>(json, Converter.Settings)!;
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Auth self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ RestSharpClient/Helpers/RestfulBooker/BookingHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Helpers;
+using RestSharpClient.ApiClient;
+using RestSharpClient.Model.RestfulBooker;
+
+namespace RestSharpClient.Helpers.RestfulBooker
+{
+	public static class BookingHelper
+	{
+        public static Booking BookingData { get; set; } = new Booking
+            {
+                Firstname = Faker.Name.First(),
+                Lastname = Faker.Name.Last(),
+                Totalprice = Faker.RandomNumber.Next(100, 1000),
+                Depositpaid = Faker.Boolean.Random(),
+                Bookingdates = new Bookingdates
+                {
+                    Checkin = DateTime.Parse("2023-01-02").ToUniversalTime().Date,
+                    Checkout = DateTime.Parse("2023-01-02").ToUniversalTime().Date
+                },
+                Additionalneeds = "Breakfast"
+            };
+
+        public static BookingIds GetRandomBookingId(IApiService client)
+        {
+            List<BookingIds> bookingIds = client
+                .GetMethod(ConfigurationHelper.BuildConfiguration().RestfulBooker.Resources.Booking)
+                .GetValueInList<BookingIds>();
+            BookingIds bookingId = bookingIds.Take(6).FirstOrDefault()!;
+            return bookingId;
+        }
+
+        public static Dictionary<string, string> GenerateBookingQueryParams(IApiService client, string[] keys)
+        {
+            Dictionary<string, string> queryParams = new();
+
+            BookingIds bookingId = GetRandomBookingId(client);
+
+            ApiMethodParams apiMethodParams = new()
+            {
+                Uri = ConfigurationHelper.BuildConfiguration().RestfulBooker.Resources.BookingId,
+                UrlSegments = new Dictionary<string, string>() { { "id", bookingId.Bookingid.ToString()! } }
+            };
+            Booking booking = client
+                .GetMethod(apiMethodParams)
+                .GetValue<Booking>();
+
+            keys.ToList().ForEach(key =>
+            {
+                switch (key)
+                {
+                    case "firstName":
+                        queryParams.Add(key, booking.Firstname);
+                        break;
+                    case "lastName":
+                        queryParams.Add(key, booking.Lastname);
+                        break;
+                    case "checkin":
+                        queryParams.Add(key, booking.Bookingdates.Checkin.ToString());
+                        break;
+                    case "checkout":
+                        queryParams.Add(key, booking.Bookingdates.Checkout.ToString());
+                        break;
+                }
+            });
+            return queryParams;
+        }
+    }
+}
+
